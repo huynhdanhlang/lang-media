1bd8f54857771bc655440f63857d5b2550dd5abf
Lang Huynh <lang.huynh@siliconstack.com.au>
2023-05-25 09:54:04 +0700

    remove migration tool file using package instead

--- apps/backend/src/utils/migration-tool/makeColumnName.ts
+++
@@ -1,5 +0,0 @@
-import { snakeCase } from "./snakeCase";
-
-export const makeColumnName = (classPropertyName: string, useSnakeCase?: boolean): string => {
-  return useSnakeCase ? snakeCase(classPropertyName) : classPropertyName;
-};
