f82d82ccf5a1c81514e4e650967956239f11d036
huynhdanhlang <danhlangbmvl@gmail.com>
2023-06-05 23:44:02 +0700

    [BE] -- Now migration with transaction options is added and update references

--- apps/backend/src/database/db_migrate.ts
+++ apps/backend/src/database/db_migrate.ts
@@ -3,12 +3,15 @@ import { sequelize } from './config';
 import { SequelizeTypescriptMigration } from '@huynhdanhlang/sequelize-typescript-migration';
 
 const bootstrap = async (migrationName: string) => {
-  await SequelizeTypescriptMigration.makeMigration(sequelize, {
-    outDir: join(__dirname, './db/migrations'),
-    migrationName: migrationName || `${Date.now().toString()}_auto_generate`,
-    preview: false,
-    debug: true,
-    useSnakeCase: false,
+  await sequelize.transaction(async (t) => {
+    await SequelizeTypescriptMigration.makeMigration(sequelize, {
+      outDir: join(__dirname, './db/migrations'),
+      migrationName: migrationName || `${Date.now().toString()}_auto_generate`,
+      preview: false,
+      debug: true,
+      useSnakeCase: false,
+      transaction: t,
+    });
   });
 };
 const migrationName: string = process.argv.slice(2).join().replace(/[,]/g, ' ');
